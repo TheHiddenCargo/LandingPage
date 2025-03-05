bcf82c3809c5b96a598e97e39dc1c4d538a7238a
koket987 <papaletagg67@gmail.com>
2025-03-05 18:13:18 -0500

    Fixing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Landing from "./components/LandingPage/Landing";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
