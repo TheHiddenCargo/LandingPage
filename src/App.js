b3b0b05ae1262bb60a9c5fe80c5f9124e0c95abe
koket987 <papaletagg67@gmail.com>
2025-03-05 18:11:28 -0500

    Prueba Sonar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Landing from "./components/LandingPage/Landing";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
