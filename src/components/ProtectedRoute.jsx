2aeabe6d263ac25465d44409fabb4a599663793b
koket987 <papaletagg67@gmail.com>
2025-03-11 09:48:37 -0500

    Fixing Route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,6 @@
 // src/components/ProtectedRoute.jsx
 import React from "react";
+import PropTypes from "prop-types";
 import { useIsAuthenticated } from "@azure/msal-react";
 import { Navigate } from "react-router-dom";
 
@@ -9,6 +10,10 @@ const ProtectedRoute = ({ children }) => {
     return <Navigate to="/" replace />;
   }
   return children;
+};
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default ProtectedRoute;
