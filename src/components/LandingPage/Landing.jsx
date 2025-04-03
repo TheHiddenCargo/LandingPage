bff915d2817f3b36cb9a0f7a05dedbc8d82fa7d5
Juanse2347 <juan.sanchez-mo@escuelaing.edu.co>
2025-04-03 00:32:03 -0500

    Fixing GUI LandingPage & Lobby

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -42,7 +42,7 @@ const Landing = () => {
                 <img src={logoImage} alt="Logo de la Empresa" className="logo" />
                 <p className="description">
                     The Hidden Cargo es un emocionante juego multijugador inspirado en el concepto de
-                    ¿Quién da más? (Storage Wars), donde los jugadores compiten en subastas para adquirir
+                    ¿Quién da más? "Storage Wars", donde los jugadores compiten en subastas para adquirir
                     bodegas de almacenamiento con contenido desconocido. Sin poder inspeccionarlas a fondo, deberán arriesgarse y aplicar estrategias para hacer ofertas inteligentes y descubrir si han conseguido artículos valiosos o simples desperdicios. A medida que ganan dinero virtual, pueden seguir participando en nuevas subastas, mejorar sus tácticas y subir de nivel, creando y vendiendo sus propias bodegas para desafiar a otros jugadores en esta dinámica experiencia de comercio y apuesta.
                 </p>
             </div>
